4cf2faf4b324ad8ca94bca2f607f27cea021db0f
李端容 <qawsed123144@Tuan.local>
2025-07-09 13:11:16 +0800

    port

--- backend/server.js
+++ backend/server.js
@@ -251,7 +251,7 @@ app.patch('/mindmap/:id', authenticateToken, async (req, res) => {
 });
 
 // 啟動
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
